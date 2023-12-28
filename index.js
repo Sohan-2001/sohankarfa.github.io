bd60a2c2e126d7eb5fb4506be7f3be23ba286a6e
Sohan-2001 <112119230+Sohan-2001@users.noreply.github.com>
2023-12-29 04:39:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,373 +1,48 @@
-<!doctype html>
-<html lang="en">
-  <head>  
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide">
-
-<link href='https://fonts.googleapis.com/css?family=Kaushan Script' rel='stylesheet'>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-     .row::after {
-  content: "";
-  clear: both;
-  display: block;
+var a=0;
+var b='a boy of fancies';
+var speed=100;
+window.onload=function typeWriter()
+{
+if(a<b.length){
+  document.getElementById("typing").innerHTML+=b.charAt(a);
+  a++;
+  setTimeout(typeWriter,speed);
 }
-      [class*="col-"] {
-        float: left;
-        padding: 10px;
-      }
-/* For desktop: */
-.col-1 {width: 8.33%;}
-.col-2 {width: 16.66%;}
-.col-3 {width: 45%;}
-.col-4 {width: 33.33%;}
-.col-5 {width: 41.66%;}
-.col-6 {width: 50%;}
-.col-7 {width: 58.33%;}
-.col-8 {width: 66.66%;}
-.col-9 {width: 75%;}
-.col-10 {width: 83.33%;}
-.col-11 {width: 91.66%;}
-.col-12 {width: 100%;}
-@media only screen and (max-width: 1000px) {
-  /* For mobile phones: */
-  [class*="col-"] {
-    width: 100%;
-    text-align: center;
-    
-    
+}
+
+var today = new Date();
+var time = today.getHours();
+if((time>=4)&&(time<=11)){
+  var Greet=document.getElementById('Time').innerHTML="Good Morning";
+}
+else if((time>11)&&(time<=16)){
+  var Greet=document.getElementById('Time').innerHTML="Good Noon";
+}
+else if((time>16)&&(time<=17)){
+  var Greet=document.getElementById('Time').innerHTML="Good Afternoon";
+}
+else if((time>17)&&(time<=18)){
+  var Greet=document.getElementById('Time').innerHTML="Good Evening";
+}
+else{
+  var Greet=document.getElementById('Time').innerHTML="Good Night";
+}
+
+
+/*
+window.onload=function()
+{
+    var i = 0;
+    var txt = '<Welcome>';
+    var speed = 200;
+    var x = document.getElementById("myAudio"); 
+    x.play();
+  if (i < txt.length) {
+    document.getElementById("type").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
   }
+    var element = document.getElementById("loaded");
+   element.classList.toggle("pre");
 }
-
-
-    </style>
-
-     </head>
-    <title>Sohan Karfa</title>
-    
- 
-  <body>
-    <!--
-    <div class="pre">
-      <div id="type">
-        <audio id="myAudio">
-            <source src="typing.mp3" type="audio/mpeg">
-        </audio>
-      </div>
-      
-      
-    </div>
-    -->
-    
-    <div class="loaded">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary" >
-  <div class="container-fluid"  >
-    <a class="navbar-brand">@</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" class="btn" aria-current="page" href="index.html">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" class="btn" href="#">Skills</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link"  class="btn" href="#">Projects</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" class="btn" href="#">Contacts</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-
-<div class="row">
-    <div class="col-3">
-      <img src="image.png" 
-      width="80%" height="auto" >
-    </div>
-    <div class="col-3">
-      <div id="typing" style="font-family:'Audiowide',sans-serif; font-size:calc(12px + 3vw); position: relative; top:3vw; text-align: center;"></div>
-      <div style="display: inline-block; position: relative; top: 6vw; text-align: center; font-family:'Audiowide',sans-serif; color: rgb(73, 123, 124); font-weight: 1px; font-size:calc(12px+1.5vw); ">
-        <a >"<p id="Time"; style="display: inline-block;"></p><br>I am Sohan Karfa. I am from Tarakeswar, Hooghly, West Bengal, India, Asia, Earth, Milky Way, Cosmos. 
-          I believe in works rather than words. I have few hobbies like writing diary, photography, chess. I am a bad leader as I don't trust others for my
-           task completion, but I am a good team member as I complete my own task quickly.
-          "</a>
-      </div>
-      
-  </div>  
-</div>
-<br><br>
-
-<h2  style="font-family:'Audiowide',sans-serif; text-align: center; background-color: rgb(216, 211, 226);  color:rgb(46, 42, 42); width: 100vw;">My Education</h2>
-<br>
-<div style="background-color: rgb(245, 245, 245); width: 100vw; font-size:calc(12px + 1vw);">
-  <div class="container text-center" style="font-family:'Audiowide',sans-serif; "  >
-    <div class="row">
-      <div class="col" >
-        <img src="https://images.shiksha.com/mediadata/images/1488259134phpShnPJa.jpeg" class="rounded"
-      width="100%" height="80%">
-      </div>
-      <div class="col">
-        <a>Lovely Professional University<br><br>
-        Punjab, India</a>
-      </div>
-      <div class="col">
-        <a><li>Degree: B.Tech CSE</li>
-        <li>Currently Studying</li>
-      <li>2025 Pass Out</li></a>
-      </div>
-      
-      </div>
-    </div>
-      </div>
-<br><br>
-
-<h2  style="font-family:'Audiowide',sans-serif; text-align: center; background-color: rgb(216, 211, 226);  color:rgb(46, 42, 42); width: 100vw;">I have learnt</h2>
-
-
-<div style="background-color: rgb(245, 245, 245); width: 100vw;">
-<div class="container text-center" style="font-family:'Audiowide',sans-serif; "  >
-  <div class="row">
-    <div class="col" >
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/1853px-C_Programming_Language.svg.png" class="rounded"
-    width="80vw" height="80vw">
-    </div>
-    <div class="col">
-      <div >
-      <h4>C Programming Language</h4>
-    </div>
-    </div>
-    
-    </div>
-  </div>
-    </div>
-
-
-<div style="height: 5px;  background-color: rgb(237, 225, 225);"></div>
-<div style="background-color: rgb(233, 235, 226); width: 100vw;">
-<div class="container text-center" style="font-family:'Audiowide',sans-serif; "  >
-  <div class="row">
-    <div class="col" >
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png" class="rounded"
-    width="80vw" height="80vw">
-    </div>
-    <div class="col">
-      <div >
-      <h4>C++ Programming Language</h4>
-    </div>
-    </div>
-    
-    </div>
-  </div>
-    </div>
-
-
-<div style="height: 5px;  background-color: rgb(237, 225, 225);"></div>
-<div style="background-color: rgb(245, 245, 245); width: 100vw;">
-<div class="container text-center" style="font-family:'Audiowide',sans-serif; "  >
-  <div class="row">
-    <div class="col" >
-      <img src="https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg" class="rounded"
-    width="80vw" height="80vw">
-    </div>
-    <div class="col">
-      <div >
-      <h4>Python Programming Language</h4>
-    </div>
-    </div>
-    
-    </div>
-  </div>
-    </div>
-
-    <div style="height: 5px;  background-color: rgb(237, 225, 225);"></div>
-<div style="background-color:  rgb(233, 235, 226); width: 100vw;">
-<div class="container text-center" style="font-family:'Audiowide',sans-serif; "  >
-  <div class="row">
-    <div class="col" >
-      <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/800px-Java_programming_language_logo.svg.png" class="rounded"
-    width="70vw" height="100hw">
-    </div>
-    <div class="col">
-      <div >
-      <h4>Java Programming Language</h4>
-    </div>
-    </div>
-    
-    </div>
-  </div>
-    </div>
-
-    <div style="height: 5px;  background-color: rgb(237, 225, 225);"></div>
-<div style="background-color:  rgb(245, 245, 245); width: 100vw;">
-<div class="container text-center" style="font-family:'Audiowide',sans-serif; "  >
-  <div class="row">
-    <div class="col" >
-      <img src="https://www.ankitweblogic.com/javascript/js_img/javascript.png" class="rounded"
-    width="80vw" height="80vw">
-    </div>
-    <div class="col">
-      <div >
-      <h4>JavaScript Programming Language</h4>
-    </div>
-    </div>
-   
-    </div>
-  </div>
-    </div>
-
-    <div style="height: 5px;  background-color: rgb(237, 225, 225);"></div>
-<div style="background-color: rgb(233, 235, 226); width: 100vw;">
-<div class="container text-center" style="font-family:'Audiowide',sans-serif; "  >
-  <div class="row">
-    <div class="col" >
-      <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png" class="rounded"
-    width="80vw" height="80vw">
-    </div>
-    <div class="col">
-      <div >
-      <h4>SQL Programming Language</h4>
-    </div>
-    </div>
-    
-    </div>
-  </div>
-    </div>
-
-  
-<div style="height: 5px;  background-color: rgb(237, 225, 225);"></div>
-<div style="background-color: rgb(245, 245, 245); width: 100vw;">
-<div class="container text-center" style="font-family:'Audiowide',sans-serif; "  >
-  <div class="row">
-    <div class="col" >
-      <img src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg" class="rounded"
-    width="80vw" height="80vw">
-    </div>
-    <div class="col">
-      <div >
-      <h4>HTML Markup Language</h4>
-    </div>
-    </div>
-    
-    </div>
-  </div>
-    </div>
-
-
-<div style="height: 5px;  background-color: rgb(237, 225, 225);"></div>
-<div style="background-color: rgb(233, 235, 226); width: 100vw;">
-<div class="container text-center" style="font-family:'Audiowide',sans-serif; "  >
-  <div class="row">
-    <div class="col" >
-      <img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg" class="rounded"
-    width="80vw" height="80vw">
-    </div>
-    <div class="col">
-      <div >
-      <h4>CSS Sheet Style Language</h4>
-    </div>
-    </div>
-    
-    </div>
-  </div>
-    </div>
-
-
-    <div style="height: 5px;  background-color: rgb(237, 225, 225);"></div>
-    <div style="background-color: rgb(245, 245, 245); width: 100vw;">
-    <div class="container text-center" style="font-family:'Audiowide',sans-serif; "  >
-      <div class="row">
-        <div class="col" >
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1024px-Bootstrap_logo.svg.png" class="rounded"
-        width="80vw" height="80vw">
-        </div>
-        <div class="col">
-          <div >
-          <h4>Bootstrap CSS Framework</h4>
-        </div>
-        </div>
-        
-        </div>
-      </div>
-        </div>
-
-
-<div style="height: 5px;  background-color: rgb(237, 225, 225);"></div>
-<div style="background-color: rgb(233, 235, 226); width: 100vw;">
-<div class="container text-center" style="font-family:'Audiowide',sans-serif; "  >
-  <div class="row">
-    <div class="col" >
-      <img src="https://odonno.gallerycdn.vsassets.io/extensions/odonno/new-cmd/0.2.0/1474455255247/Microsoft.VisualStudio.Services.Icons.Default" class="rounded"
-    width="80vw" height="80vw">
-    </div>
-    <div class="col">
-      <div >
-      <h4>Command Prompt Shell Scripting Language</h4>
-    </div>
-    </div>
-    
-    </div>
-  </div>
-
-
-  <div style="height: 5px;  background-color: rgb(237, 225, 225);"></div>
-  <div style="background-color: rgb(245, 245, 245); width: 100vw;">
-  <div class="container text-center" style="font-family:'Audiowide',sans-serif; "  >
-    <div class="row">
-      <div class="col" >
-        <img src="https://hackernoon.imgix.net/hn-images/1*hLnluojxspNir60RyS8wuQ.png" class="rounded"
-      width="100%" height="100%">
-      </div>
-      <div class="col">
-        <div >
-        <h4>Machine Learning With Python</h4>
-      </div>
-      </div>
-      
-      
-      </div>
-    </div>
-      </div>
-
-
-      <div style="height: 5px;  background-color: rgb(237, 225, 225);"></div>
-<div style="background-color: rgb(233, 235, 226); width: 100vw;">
-<div class="container text-center" style="font-family:'Audiowide',sans-serif; "  >
-  <div class="row">
-    <div class="col" >
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Android_Studio_Logo_%282023%29.svg/1024px-Android_Studio_Logo_%282023%29.svg.png" class="rounded"
-    width="120vw" height="120vw">
-    </div>
-    <div class="col">
-      <div >
-      <h4>Android Development - Java & Kotlin</h4>
-    </div>
-    </div>
-    <div class="col">
-      <h4 >Tool: Android Studio</h4>
-    </div>
-   
-    </div>
-  </div>
-
-
-    </div><div style="height: 35px;  background-color: rgb(216, 211, 226);"></div>
-
-
-
-
-
-
-
-</div>
-</div>
-<script src="index.js"></script>
-</body>
-</html>
+*/
