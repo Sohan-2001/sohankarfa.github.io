7a1e2dce99f36cfcfd108df03da54b73fda8ff00
Sohan-2001 <112119230+Sohan-2001@users.noreply.github.com>
2024-01-09 04:54:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,66 @@
-.Btn,.Btn1 {
-    border-radius: 10px;
-    background-color: rgb(202, 216, 200);
-    border-color: rgb(202, 216, 200);
-    color: rgb(33, 79, 24);
+var a=0;
+var b='a boy of fancies';
+var speed=100;
+window.onload=function typeWriter()
+{
+if(a<b.length){
+  document.getElementById("typing").innerHTML+=b.charAt(a);
+  a++;
+  setTimeout(typeWriter,speed);
+}
+}
 
+var today = new Date();
+var time = today.getHours();
+if((time>=4)&&(time<=11)){
+  var Greet=document.getElementById('Time').innerHTML="Good Morning";
+}
+else if((time>11)&&(time<=16)){
+  var Greet=document.getElementById('Time').innerHTML="Good Noon";
+}
+else if((time>16)&&(time<=17)){
+  var Greet=document.getElementById('Time').innerHTML="Good Afternoon";
+}
+else if((time>17)&&(time<=18)){
+  var Greet=document.getElementById('Time').innerHTML="Good Evening";
+}
+else{
+  var Greet=document.getElementById('Time').innerHTML="Good Night";
+}
+
+
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+      document.querySelector(
+          "body").style.visibility = "hidden";
+      document.querySelector(
+          "#loader").style.visibility = "visible";
+  } else {
+      document.querySelector(
+          "#loader").style.display = "none";
+      document.querySelector(
+          "body").style.visibility = "visible";
   }
-  
-._active, .Btn:hover,._active1, .Btn1:hover {
-    border-radius: 0px 10px 0px 10px;
-    background-color: rgb(118, 163, 110);
-    border-color: rgb(118, 163, 110);
-    color: rgb(255, 255, 255);
+};
 
-  }
 
-.Skills{
-  background-color: rgb(236, 253, 236);
-   box-shadow: 20px 20px 5px rgb(188, 208, 188);
-   border-radius: 10px; 
+var header = document.getElementById("carousel");
+var btns = header.getElementsByClassName("Btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("_active");
+  current[0].className = current[0].className.replace(" _active", "");
+  this.className += " _active";
+  });
 }
-.SkillBox{
-  background-color:rgb(178, 207, 178);
-  border-radius: 10px;
-  box-shadow: 20px 20px 5px rgb(146, 195, 203);
-  border-radius: 10px;
+var header = document.getElementById("carousel1");
+var btns = header.getElementsByClassName("Btn1");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("_active1");
+  current[0].className = current[0].className.replace(" _active1", "");
+  this.className += " _active1";
+  });
 }
-.Skills:hover, .Skills_Active{
-  background-color:rgb(178, 207, 178);
-   box-shadow: 5px 5px rgb(188, 208, 188);
-   border-radius: 10px; 
-}
+
+
